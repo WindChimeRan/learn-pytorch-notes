ef46a9dffe6af240d9f5c029ac26ca4d8b0f4d45
windChimeRan <1012869439@qq.com>
2017-06-15 10:22:34 +0800

    generator

--- rf_ann_wine.py
+++ rf_ann_wine.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 
 
+batch_size = 10
+steps = 1000
+
 # data
 if not os.path.exists('wine/wine.data'):
     df_wine = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
@@ -22,56 +25,81 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.40,  random_state=1)
 
-
-import torch
-from torch import nn, optim
-from torch.autograd import Variable
-
-classifier = nn.Sequential(
-    nn.Linear(14,28),
-    nn.Tanh(),
-    nn.Linear(28, 28),
-    nn.Tanh(),
-    nn.Linear(28, 3),
-    nn.Sigmoid()
-)
-X_train = np.array(X_train,dtype=np.float32)
-X_train = torch.from_numpy(X_train)
-y_train = torch.from_numpy(y_train)
-y_train = Variable(y_train)
-X_train = Variable(X_train)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(classifier.parameters(),lr=1e-3)
-
-for i in range(1000):
-
-    X_train = X_train.view(-1, 14)
-    out = classifier(X_train)
-    loss = criterion(out, y_train)
+data = (X_train, X_test, y_train, y_test )
 
 
+def nn_classifier(data):
+
+    import torch
+    from torch import nn, optim
+    from torch.autograd import Variable
+    X_train, X_test, y_train, y_test = data
+    X_train = np.array(X_train,dtype=np.float32)
+    X_test = np.array(X_test,dtype=np.float32)
+    y_train = Variable(torch.from_numpy(y_train)).cuda()
+    X_train = Variable(torch.from_numpy(X_train)).cuda()
+    y_test = Variable(torch.from_numpy(y_test)).cuda()
+    X_test = Variable(torch.from_numpy(X_test)).cuda()
+
+    classifier = nn.Sequential(
+        nn.Linear(14,28),
+        nn.Tanh(),
+        nn.Linear(28, 28),
+        nn.Tanh(),
+        nn.Linear(28, 3),
+        nn.Sigmoid()
+    )
+
+
+    classifier.cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(classifier.parameters(),lr=1e-3)
+
+    def data_loader(dataset):
+        x,y = dataset
+        def data_batch(batch_size,steps):
+
+            for i in range(steps):
+                t = np.random.randint(0, 104 - batch_size)
+                yield x[t:batch_size+t,:],y[t:batch_size+t]
+        return data_batch
+
+    train_data = data_loader((X_train,y_train))
+
+    for i,(x,y) in enumerate(train_data(batch_size,steps)):
+
+        out = classifier(x)
+        loss = criterion(out, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+    classifier.eval()
+    out = classifier(X_test)
     _, pred = torch.max(out, 1)
-    num_correct = (pred == y_train).sum()
-    running_acc = num_correct.data[0]
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print running_acc/104.0
-
-# random forest
-# from sklearn.ensemble import RandomForestClassifier
-# rf = RandomForestClassifier(n_estimators=1000, criterion='gini', max_features='sqrt', max_depth=None, min_samples_split=2, bootstrap=True, n_jobs=1, random_state=1)
-# rf = rf.fit(X_train, y_train)
-#
-# # inference
-# y_train_pred = rf.predict(X_train)
-# y_test_pred = rf.predict(X_test)
-
-# estimate
-# tree_train = accuracy_score(y_train, y_train_pred)
-# tree_test = accuracy_score(y_test, y_test_pred)
+    num_correct = (pred == y_test).sum()
+    running_acc = (num_correct.data[0]+0.0)/X_test.size()[0]
+    print "nn ",running_acc
 
 
-# print('Random Forest train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+def rf(data):
+    X_train, X_test, y_train, y_test = data
+# # random forest
+    from sklearn.ensemble import RandomForestClassifier
+    rf = RandomForestClassifier(n_estimators=1000, criterion='gini', max_features='sqrt', max_depth=None, min_samples_split=2, bootstrap=True, n_jobs=1, random_state=1)
+    rf = rf.fit(X_train, y_train)
+
+    # inference
+    y_train_pred = rf.predict(X_train)
+    y_test_pred = rf.predict(X_test)
+
+    #estimate
+    tree_train = accuracy_score(y_train, y_train_pred)
+    tree_test = accuracy_score(y_test, y_test_pred)
+
+
+    print('Random Forest train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+if __name__ == '__main__':
+    nn_classifier(data)
+    rf(data)
